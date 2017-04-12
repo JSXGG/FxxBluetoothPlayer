906e792906b015ad083217e7b85a4e19f14f87cb
Baird-weng <644672334@qq.com>
2017-04-12 23:10:55 +0800

    no message

---
+++ FxxBluetoothPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FxxBluetoothPlayer
+//
+//  Created by Baird-weng on 2017/4/12.
+//  Copyright © 2017年 Baird-weng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, ‘8.0’
+target 'FxxBluetoothPlayer' do
+pod "AFNetworking"
+end
